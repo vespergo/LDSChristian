ff779cbe8f7a73febacc72f85c31c0a7f31e78e8
Josh O'Bryan <vespergo@gmail.com>
2026-05-28 22:44:07 -0400

    added actions

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
   return {
     templateFormats: ["njk", "html"],
     htmlTemplateEngine: "njk",
+    pathPrefix: "/LDSChristian/",
     dir: {
       input: "src",
       output: "_site",
